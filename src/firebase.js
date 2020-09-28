08af2814cb00c895ecd581805e65328350df1939
Mohit Pant <MOHITPAN@amdocs.com>
2020-09-28 21:59:19 +0300

    added header ui

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBI3DHdBblJddhfHUKBzjiGqyRw35CvvrI",
+    authDomain: "clone-react-d7f87.firebaseapp.com",
+    databaseURL: "https://clone-react-d7f87.firebaseio.com",
+    projectId: "clone-react-d7f87",
+    storageBucket: "clone-react-d7f87.appspot.com",
+    messagingSenderId: "502356229097",
+    appId: "1:502356229097:web:60ef143c881d3bce192978"
+};
