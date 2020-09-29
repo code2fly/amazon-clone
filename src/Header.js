19cb820f937e2892db16d34a3edcc55a6c693b01
Mohit Pant <MOHITPAN@amdocs.com>
2020-09-29 12:19:57 +0300

    basic checkout page added without data layer(context) integration

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
+
 import './Header.css';
 
 function Header() {
     return (
         <div className="header">
 
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            <Link to="/" >
+                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            </Link>
 
             <div className="header__search">
                 <input type="text" className="header__searchInput" />
@@ -30,10 +34,12 @@ function Header() {
                     <span className="header__optionLineTwo">Prime</span>
                 </div>
 
-                <div className="header_optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header__optionLineTwo header__basketCount">0</span>
-                </div>
+                <Link to="/checkout">
+                    <div className="header_optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header__optionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
             </div>
 
         </div >
