19a0a2d00b919495e4597ef9dc7004a0e6569aa1
Mohit Pant <MOHITPAN@amdocs.com>
2020-10-05 18:52:07 +0300

    setup firebase config, firestore(db), auth

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import './Login.css';
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        // do the firebase login magic here
+        console.log('firebase magic is going to start')
+    }
+
+
+    const register = e => {
+        e.preventDefault();
+
+        // do somme firebase register magic here
+        console.log('firebase register magic is going to start')
+    }
+
     return (
         <div className="login">
             <Link to="/">
@@ -13,17 +32,17 @@ function Login() {
                 <h1>Sign-In</h1>
                 <form className="login__form" action="" method="post">
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button>Sign In</button>
+                    <button type="submit" onClick={signIn} >Sign In</button>
 
                 </form>
 
                 <p>By continuing, you agree to Our FAKE Amazon's Conditions of Use and Privacy Notice</p>
 
-                <button className="login__signupButton">Create your Amazon Account</button>
+                <button className="login__signupButton" type="submit" onClick={register}>Create your Amazon Account</button>
             </div>
         </div>
     )
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyBI3DHdBblJddhfHUKBzjiGqyRw35CvvrI",
     authDomain: "clone-react-d7f87.firebaseapp.com",
@@ -7,3 +10,12 @@ const firebaseConfig = {
     messagingSenderId: "502356229097",
     appId: "1:502356229097:web:60ef143c881d3bce192978"
 };
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebaseApp.auth();
+
+export { db, auth };
